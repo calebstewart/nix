18a7909cd4bd493b5a82e36f5ca30a975d03850a
Caleb Stewart <caleb.stewart94@gmail.com>
2024-05-31 16:35:27 -0400

    Added eza

---
+++ modules/eza/default.nix
@@ -0,0 +1,17 @@
+{lib, config, ...}:
+let
+  cfg = config.modules.eza;
+in {
+  options.modules.eza = {
+    enable = lib.mkEnableOption "eza";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.eza = {
+      enable = true;
+      enableZshIntegration = true;
+      git = true;
+      icons = true;
+    };
+  };
+}
