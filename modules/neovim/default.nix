48a6a9be4f57ae0a4be75541a57684d9d572e84b
Caleb Stewart <caleb.stewart94@gmail.com>
2024-06-01 13:34:02 -0400

    Improved neovim configuration

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,4 +1,4 @@
-{lib, config, ...}:
+{lib, config, pkgs, ...}:
 let
   cfg = config.modules.neovim;
   mkIf = lib.mkIf;
@@ -11,6 +11,8 @@ in {
       enable = true;
       defaultEditor = true;
       colorschemes.base16.enable = true;
+      
+      globals.mapleader = " ";
 
       # Neovim options
       opts = {
@@ -21,6 +23,12 @@ in {
         shiftwidth = 2;
         tabstop = 2;
         softtabstop = 2;
+
+        ignorecase = true;
+        smartcase = true;
+        signcolumn = "yes";
+        cursorline = true;
+        ruler = true;
       };
 
       # Use whatever color scheme was selected for the system. This is
@@ -55,10 +63,16 @@ in {
 
       # Enable common plugins with no extra configuration
       plugins = {
+        nix.enable = true;
+        bufferline.enable = true;
         lualine.enable = true;
         lsp-format.enable = true;
+        oil.enable = true;
+        treesitter.enable = true;
+        cmp-nvim-lsp-signature-help.enable = true;
       };
 
+      # Toggle-able terminal emulators within Neovim!
       plugins.toggleterm = {
         enable = true;
 
@@ -82,12 +96,6 @@ in {
         };
       };
 
-      plugins.cmp-nvim-lsp-signature-help.enable = true;
-
-      plugins.treesitter = {
-        enable = true;
-      };
-
       # Setup auto-completion
       plugins.cmp = {
         enable = true;
@@ -100,10 +108,120 @@ in {
         ];
       };
 
+      # Setup none-ls for LSP features from external tools
       plugins.none-ls = {
         enable = true;
         enableLspFormat = true;
       };
+
+      # Install telescope because it's pretty :)
+      plugins.telescope = {
+        enable = true;
+
+        extensions = {
+          fzf-native.enable = true;
+        };
+      };
+
+      # Alpha provides a nice dashboard if no file is opened
+      plugins.alpha = {
+        enable = true;
+        theme = "dashboard";
+        iconsEnabled = true;
+      };
+
+      # Setup neotree for a file browser bar
+      plugins.neo-tree = {
+        enable = true;
+        enableDiagnostics = true;
+        enableGitStatus = true;
+        enableModifiedMarkers = true;
+        enableRefreshOnWrite = true;
+        closeIfLastWindow = true;
+        popupBorderStyle = "rounded";
+
+        window = {
+          mappings = {
+            "<space>" = "none";
+          };
+        };
+      };
+
+      extraPlugins = with pkgs.vimPlugins; [
+        vim-bbye # bbye provides nicer Bwipeout which doesn't close the window
+      ];
+
+      keymaps = [
+        # Toggle the file explorer
+        {
+          key = "<leader>of";
+          action = "<cmd>Neotree toggle<CR>";
+        }
+
+        # Toggle the terminal
+        {
+          key = "<leader>ot";
+          action = "<cmd>ToggleTerm<CR>";
+        }
+
+        # Close the current buffer
+        {
+          key = "<leader>bd";
+          action = "<cmd>Bwipeout<CR>";
+        }
+
+        # Close all buffers
+        {
+          key = "<leader>bK";
+          action = "<cmd>bufdo :Bwipeout<CR>";
+        }
+
+        # Move to the previous buffer
+        {
+          key = "<leader>bh";
+          action = "<cmd>bprevious<CR>";
+        }
+
+        # Move to the next buffer
+        {
+          key = "<leader>bl";
+          action = "<cmd>bnext<CR>";
+        }
+
+        # Swap between the two most recent buffers (either j or k)
+        {
+          key = "<leader>bk";
+          action = "<cmd>b#<CR>";
+        }
+        {
+          key = "<leader>bj";
+          action = "<cmd>b#<CR>";
+        }
+
+        # Move to the next window to the left
+        {
+          key = "<leader>wh";
+          action = "<cmd>wincmd h<CR>";
+        }
+
+        # Move to the next window down
+        {
+          key = "<leader>wj";
+          action = "<cmd>wincmd j<CR>";
+        }
+
+        # Move to the next window up
+        {
+          key = "<leader>wk";
+          action = "<cmd>wincmd k<CR>";
+        }
+
+        # Move to the next window right
+        {
+          key = "<leader>wl";
+          action = "<cmd>wincmd l<CR>";
+        }
+      ];
     };
   };
 }
