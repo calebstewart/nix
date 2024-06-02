534f20865d185b8c7169d3ac18b9cd56386462a9
Caleb Stewart <caleb.stewart94@gmail.com>
2024-06-01 20:45:58 -0400

    Cleaned neovim configuration

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -10,10 +10,12 @@ in {
     programs.nixvim = {
       enable = true;
       defaultEditor = true;
-      colorschemes.base16.enable = true;
       
       globals = {
+        # The global leader is " ", which behaves similarly to emacs shortcuts
         mapleader = " ";
+
+        # Default to a transparent background through transparent.nvim
         transparent_enabled = true;
       };
 
@@ -39,23 +41,27 @@ in {
       # set base16.colorscheme to config.colorScheme.slug, but in the
       # event we use a non-standard base16 scheme, that will fail.
       # instead, we set the scheme manually.
-      colorschemes.base16.colorscheme = with config.colorScheme.palette; {
-        base00 = "#${base00}";
-        base01 = "#${base01}";
-        base02 = "#${base02}";
-        base03 = "#${base03}";
-        base04 = "#${base04}";
-        base05 = "#${base05}";
-        base06 = "#${base06}";
-        base07 = "#${base07}";
-        base08 = "#${base08}";
-        base09 = "#${base09}";
-        base0A = "#${base0A}";
-        base0B = "#${base0B}";
-        base0C = "#${base0C}";
-        base0D = "#${base0D}";
-        base0E = "#${base0E}";
-        base0F = "#${base0F}";
+      colorschemes.base16 = {
+        enable = true;
+
+        colorscheme = with config.colorScheme.palette; {
+          base00 = "#${base00}";
+          base01 = "#${base01}";
+          base02 = "#${base02}";
+          base03 = "#${base03}";
+          base04 = "#${base04}";
+          base05 = "#${base05}";
+          base06 = "#${base06}";
+          base07 = "#${base07}";
+          base08 = "#${base08}";
+          base09 = "#${base09}";
+          base0A = "#${base0A}";
+          base0B = "#${base0B}";
+          base0C = "#${base0C}";
+          base0D = "#${base0D}";
+          base0E = "#${base0E}";
+          base0F = "#${base0F}";
+        };
       };
 
       # Enable cliboard support with the default register
@@ -75,9 +81,16 @@ in {
         cmp-nvim-lsp-signature-help.enable = true;
         transparent.enable = true;
         noice.enable = true;
-        notify.enable = true;
         neogit.enable = true;
         vim-bbye.enable = true;
+      };
+
+      # Fancy notifications within neovim
+      plugins.notify = {
+        enable = true;
+
+        # NOTE: british spelling :sob:
+        backgroundColour = "#${config.colorScheme.palette.base01}";
       };
 
       # Toggle-able terminal emulators within Neovim!
