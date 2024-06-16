9579b2e6e8aa0da4ea9effde579592b51b8b8f7b
Caleb Stewart <caleb.stewart94@gmail.com>
2024-06-16 18:40:12 -0400

    Add custom firefox package for darwin

--- hosts/huntress-mbp/user.nix
+++ hosts/huntress-mbp/user.nix
@@ -1,25 +1,52 @@
-{user, ...}: {
+{user, pkgs, ...}: 
+let
+  firefox = pkgs.stdenv.mkDerivation rec {
+    pname = "firefox";
+    version = "123.0";
+    sourceRoot = ".";
+    phases = ["unpackPhase" "installPhase"];
+
+    src = pkgs.stdenv.fetchurl {
+      name = "Firefox-${version}.dmg";
+      url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/127.0/mac/en-US/Firefox%20${version}.dmg";
+    };
+
+    buildInputs = with pkgs; [
+      undmg
+    ];
+  
+    installPhase = ''
+      mkdir -p "$out/Applications"
+      cp -r Firefox.app "$out/Applications/Firefox.app";
+    '';
+  };
+in {
   imports = [
     ../../modules/default.nix
   ];
 
-  config.modules = {
-    # Graphical User Interface (GUI)
-    firefox.enable = false;
+  config = {
+    modules = {
+      # Graphical User Interface (GUI)
+      firefox.enable = false;
 
-    # Command Line Interface (CLI)
-    neovim.enable = true;
-    zsh.enable = true;
-    git.enable = true;
-    golang.enable = true;
-    zoxide.enable = true;
-    bat.enable = true;
-    eza.enable = true;
-    # gpg.enable = true;
-    # direnv.enable = true;
+      # Command Line Interface (CLI)
+      neovim.enable = true;
+      zsh.enable = true;
+      git.enable = true;
+      golang.enable = true;
+      zoxide.enable = true;
+      bat.enable = true;
+      eza.enable = true;
+      # gpg.enable = true;
+      # direnv.enable = true;
 
-    # System
-    # xdg.enable = true;
-    # packages.enable = true;
+      # System
+      # xdg.enable = true;
+      # packages.enable = true;
+    };
+
+    # Replace the firefox package with our custom derivation
+    programs.firefox.package = firefox;
   };
 }
