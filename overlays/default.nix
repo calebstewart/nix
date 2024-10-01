c718f3e5d19582abc02033e5f036825dc310b697
Caleb Stewart <caleb.stewart94@gmail.com>
2024-10-01 09:04:49 -0400

    Removed hyprsplit overlay in favor of new upstream package

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,25 +6,21 @@ in [
   # (final: prev: with nikstur-pkgs; {
   #   systemd = systemd;
   # })
+
   (final: prev: {
-    hyprlandPlugins.hyprsplit = prev.stdenv.mkDerivation {
-      pname = "hyprsplit";
-      version = "0.1";
-      src = inputs.hyprsplit;
-
-      nativeBuildInputs = with prev; [pkg-config meson ninja];
-      buildInputs = with prev; [
-        hyprland.dev
-        pixman
-        libdrm
-      ] ++ hyprland.buildInputs;
-
-      meta = with prev.lib; {
-        homepage = "https://github.com/shezdy/hyprsplit";
-        description = "Hyprland plugin for separate sets of workspaces on each monitor";
-        license = licenses.bsd3;
-        platforms = platforms.linux;
-      };
+    steam = prev.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
     };
   })
 ]
