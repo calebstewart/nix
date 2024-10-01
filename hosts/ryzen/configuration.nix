e7e5ba97fd6ea86d4595abf7c299263cc951a57f
Caleb Stewart <caleb.stewart94@gmail.com>
2024-10-01 09:04:09 -0400

    Added steam, gamescope and gamemode

--- hosts/ryzen/configuration.nix
+++ hosts/ryzen/configuration.nix
@@ -78,5 +78,21 @@ in {
 
   config = {
     nixpkgs.config.rocmSupport = true;
+
+    programs.gamemode.enable = true;
+
+    programs.gamescope = {
+      enable = true;
+      # capSysNice = true;
+    };
+
+    programs.steam = {
+      enable = true;
+
+      remotePlay.openFirewall = true;
+      # dedicatedServer.openFirewall = true;
+      # localNetworkGameTransfers.openFirewall = true;
+    };
+
   };
 }
