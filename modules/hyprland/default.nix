5346da9ff290698aae80252f3e6a5f1388df49c0
Caleb Stewart <caleb.stewart94@gmail.com>
2024-10-13 21:44:59 -0400

    Updated system

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -142,8 +142,8 @@ in {
 
           bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
           animation = [
-            "windows, 1, 7, myBezier"
-            "windowsOut, 1, 7, default, popin 80%"
+            "windows, 1, 5, myBezier, popin 80%"
+            "windowsOut, 1, 5, myBezier, slidefade"
             "border, 1, 10, default"
             "borderangle, 1, 8, default"
             "fade, 1, 7, default"
@@ -451,11 +451,14 @@ in {
         package = pkgs.catppuccin-cursors.mochaDark;
       };
 
-      theme = {
-        name = "catppuccin-mocha-blue-standard";
-        package = pkgs.catppuccin-gtk.override {
-          variant = "mocha";
-          accents = ["blue"];
+      theme = let
+        nix-colors-lib = inputs.nix-colors.lib.contrib {
+          inherit pkgs;
+        };
+      in {
+        name = config.colorScheme.slug;
+        package = nix-colors-lib.gtkThemeFromScheme {
+          scheme = config.colorScheme;
         };
       };
       
